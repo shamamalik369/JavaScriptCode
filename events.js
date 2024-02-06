4a8343a03036ed8f218aa5eed382f217ee230b08
Your Name <you@example.com>
2024-02-06 19:51:19 +0530

    add files

---
+++ events.js
@@ -0,0 +1,13 @@
+let btn=document.querySelector("btn1")
+btn.onclick=()=>{
+    console.log("Hello , I clicked it");
+    let c=2;
+    c++;
+    console.log(c);
+};
+
+let div=document.querySelector("div");
+div.onmousemove=()=>{
+    console.log("I am div boxes");
+
+}
